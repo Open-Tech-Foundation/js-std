4d9c7091acce861c232f8b5016e6025b84629f5e
Thanga Ganapathy <ganapathy888@gmail.com>
2021-07-10 19:20:20 +0530

    Add documentation website using docusaurus

---
+++ website/docusaurus.config.js
@@ -0,0 +1,53 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'ES Utils',
+  tagline: 'A Collection of ES Utility Functions.',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  organizationName: 'open-tech-world', // Usually your GitHub org/user name.
+  projectName: 'es-utils', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: '@open-tech-world/es-utils',
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          href: 'https://github.com/open-tech-world/es-utils',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} Contributors of <a href="https://github.com/open-tech-world/es-utils">open-tech-world/es-utils</a>. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
