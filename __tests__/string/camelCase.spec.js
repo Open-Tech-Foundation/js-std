ab6dc49f1829a1d58f27715aaa111875804f7e6d
Thanga Ganapathy <ganapathy888@gmail.com>
2021-07-18 23:44:13 +0530

    Fix camelCase issue for returning cap words

--- __tests__/string/camelCase.spec.js
+++ __tests__/string/camelCase.spec.js
@@ -3,25 +3,29 @@ import { camelCase } from '../../lib/index.js';
 describe('String', () => {
   test('camelCase', () => {
     expect(() => camelCase()).toThrow();
-    expect(camelCase('a')).toMatch('a');
-    expect(camelCase('A')).toMatch('a');
-    expect(camelCase('aB')).toMatch('aB');
-    expect(camelCase('AB')).toMatch('aB');
-    expect(camelCase('abc')).toMatch('abc');
-    expect(camelCase('i phone')).toMatch('iPhone');
-    expect(camelCase('i-phone')).toMatch('iPhone');
-    expect(camelCase('i_phone')).toMatch('iPhone');
-    expect(camelCase('i.phone')).toMatch('iPhone');
-    expect(camelCase('The Quick Brown Fox')).toMatch('theQuickBrownFox');
-    expect(camelCase('TheQuickBrownFox')).toMatch('theQuickBrownFox');
-    expect(camelCase('The quick.brown_fox-jumps over the lazy-dog.')).toMatch(
+    expect(camelCase('a')).toBe('a');
+    expect(camelCase('A')).toBe('a');
+    expect(camelCase('aB')).toBe('aB');
+    expect(camelCase('AB')).toBe('ab');
+    expect(camelCase('abc')).toBe('abc');
+    expect(camelCase('i phone')).toBe('iPhone');
+    expect(camelCase('i-phone')).toBe('iPhone');
+    expect(camelCase('i_phone')).toBe('iPhone');
+    expect(camelCase('i.phone')).toBe('iPhone');
+    expect(camelCase('The Quick Brown Fox')).toBe('theQuickBrownFox');
+    expect(camelCase('The Quick BROWNFox')).toBe('theQuickBrownFox');
+    expect(camelCase('The Quick BROWNFoxJUMPS')).toBe('theQuickBrownFoxJumps');
+    expect(camelCase('TheQuickBrownFox')).toBe('theQuickBrownFox');
+    expect(camelCase('The quick.brown_fox-jumps OVER the lazy-dog.')).toBe(
       'theQuickBrownFoxJumpsOverTheLazyDog'
     );
-    expect(camelCase('--foo.bar')).toMatch('fooBar');
-    expect(camelCase('Foo Bar')).toMatch('fooBar');
-    expect(camelCase('__FOO_BAR__')).toMatch('fooBar');
-    expect(camelCase('foo,bar')).toMatch('fooBar');
-    expect(camelCase('1apple')).toMatch('1apple');
-    expect(camelCase('1 apple')).toMatch('1Apple');
+    expect(camelCase('--foo.bar')).toBe('fooBar');
+    expect(camelCase('Foo Bar')).toBe('fooBar');
+    expect(camelCase('__FOO_BAR__')).toBe('fooBar');
+    expect(camelCase('foo,bar')).toBe('fooBar');
+    expect(camelCase('1apple')).toBe('1apple');
+    expect(camelCase('1 apple')).toBe('1Apple');
+    expect(camelCase('fooBAR')).toBe('fooBar');
+    expect(camelCase('fooBARBaz')).toBe('fooBarBaz');
   });
 });
--- src/string/camelCase.ts
+++ src/string/camelCase.ts
@@ -2,19 +2,38 @@ import capitalize from './capitalize';
 import replaceAt from './replaceAt';
 
 function camelCase(str: string): string {
-  const result = str.match(/[a-z0-9]+/gi);
+  let out;
+  const res = str.match(/[a-z0-9]+/gi);
 
-  if (result) {
-    if (result.length === 1) {
-      return replaceAt(result[0], 0, result[0][0].toLowerCase());
+  const wordCase = (word: string) => {
+    const regexp = new RegExp(/[A-Z]+/g);
+    let match;
+
+    while ((match = regexp.exec(word)) !== null) {
+      if (regexp.lastIndex && regexp.lastIndex < word.length) {
+        word =
+          word.substring(0, match.index) +
+          capitalize(word.substring(match.index, regexp.lastIndex - 1)) +
+          word.substring(regexp.lastIndex - 1);
+      } else {
+        return word.substring(0, match.index) + capitalize(match[0]);
+      }
     }
 
-    result[0] = replaceAt(result[0], 0, result[0].toLowerCase());
-    for (let i = 1; i < result.length; i++) {
-      result[i] = capitalize(result[i]);
-    }
+    return word;
+  };
 
-    return result.join('');
+  if (res) {
+    for (let i = 0; i < res.length; i++) {
+      if (res[i].match(/[A-Z]+/g)) {
+        res[i] = wordCase(res[i]);
+      } else {
+        res[i] = capitalize(res[i]);
+      }
+    }
+    out = res.join('');
+
+    return replaceAt(out, 0, out[0].toLowerCase());
   }
 
   return str;
