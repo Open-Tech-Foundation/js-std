a5b545aecb491344d9be88aedba7469a2f4430e6
Thanga Ganapathy <ganapathy888@gmail.com>
2024-03-12 00:35:22 +0530

    docs: show repl only when view intersect

--- apps/docs/components/REPL.js
+++ apps/docs/components/REPL.js
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 import "@opentf/react-node-repl/lib/style.css";
 import { useTheme } from "next-themes";
 import Link from "next/link";
+import { useInView } from "react-intersection-observer";
 
 const NodeREPL = dynamic(
   () => import("@opentf/react-node-repl").then((mod) => mod.NodeREPL),
@@ -13,31 +14,40 @@ const setupCode = `const _ = require('lodash');
 const log = console.log;`;
 
 export default function REPL({ code = "", layout = "DEFAULT" }) {
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 1,
+    triggerOnce: true,
+  });
   const { resolvedTheme } = useTheme();
 
   return (
-    <div style={{ padding: "5px" }}>
-      <NodeREPL
-        code={code}
-        setupCode={setupCode}
-        deps={["@opentf/utils", "lodash"]}
-        editor={{
-          darkMode: resolvedTheme === "dark",
-          header: false,
-          style: { minHeight: "150px", maxHeight: "300px" },
-        }}
-        terminal={{
-          show: false,
-          style: { minHeight: "150px", maxHeight: "300px" },
-        }}
-        console={{ style: { minHeight: "150px", maxHeight: "300px" } }}
-      />
-      <div style={{ textAlign: "right", fontSize: "14px" }}>
-        ⚡ by{" "}
-        <Link href="https://node-repl.pages.dev/" target="_blank">
-          React Node REPL
-        </Link>
-      </div>
+    <div ref={ref}>
+      {inView && (
+        <div style={{ padding: "5px" }}>
+          <NodeREPL
+            code={code}
+            setupCode={setupCode}
+            deps={["@opentf/utils", "lodash"]}
+            editor={{
+              darkMode: resolvedTheme === "dark",
+              header: false,
+              style: { minHeight: "150px", maxHeight: "300px" },
+            }}
+            terminal={{
+              show: false,
+              style: { minHeight: "150px", maxHeight: "300px" },
+            }}
+            console={{ style: { minHeight: "150px", maxHeight: "300px" } }}
+          />
+          <div style={{ textAlign: "right", fontSize: "14px" }}>
+            ⚡ by{" "}
+            <Link href="https://node-repl.pages.dev/" target="_blank">
+              React Node REPL
+            </Link>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
